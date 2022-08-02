c439becf31795329bd7c1b8e820c4477bff0ae9d
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-02 21:30:47 +0100

    Update my_reconstruction.py

--- my_reconstruction.py
+++ my_reconstruction.py
@@ -90,8 +90,8 @@ if __name__ == '__main__':
         pbar = tqdm.tqdm(total=len(files), unit='File', unit_scale=True)
 
         for i_file, file_name in enumerate(files):
-            # if not file_name == "17-04-13_15-05-43_3599500000_3659500000":
-            #     continue
+            if not file_name == "17-04-13_15-05-43_3599500000_3659500000":
+                continue
             # if not file_name == "moorea_2019-06-26_test_02_000_976500000_1036500000":
             #     continue
             event_file = os.path.join(root, file_name + '_td.dat')
