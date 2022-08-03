2885dcf9f599885e7de507e03a627bd753d91fc3
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-03 01:37:53 +0100

    Update my_reconstruction.py

--- my_reconstruction.py
+++ my_reconstruction.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         shape = [240,304]
         target_shape = [256, 320]
     #events_window = 500000
-    events_window = int(shape[0] * shape[1] * 0.35)
+    events_window = int(shape[0] * shape[1])
 
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
@@ -84,8 +84,8 @@ if __name__ == '__main__':
 
         for i_file, file_name in enumerate(files):
 
-            # if not file_name == "17-04-13_15-05-43_2074500000_2134500000":
-            #     continue
+            if not file_name == "17-04-13_15-05-43_2074500000_2134500000":
+                continue
             # if not file_name == "moorea_2019-06-26_test_02_000_976500000_1036500000":
             #     continue
 
